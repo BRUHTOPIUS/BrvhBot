b786eaf438a279dcb10370e6ad0082e3500d5624
Brvh <87946746+BRUHTOPIUS@users.noreply.github.com>
2021-07-30 22:37:53 +0200

    Add bot boilerplate

--- bot.js
+++ bot.js
@@ -1,1 +1,10 @@
+const { Client } = require("discord.js");
 const {token} = require("./token.json");
+
+const bot = new Client();
+
+bot.on("ready", () => {
+    console.log("Ready to faithfully serve the Repvblic!");
+});
+
+bot.login(token);
