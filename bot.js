f4f6ccebcf811b911f556e36a3eabd23cf1dd0d3
Brvh <87946746+BRUHTOPIUS@users.noreply.github.com>
2021-07-30 23:49:37 +0200

    Add sneed command

--- bot.js
+++ bot.js
@@ -1,5 +1,17 @@
 const { Client, RichEmbed, MessageEmbed, MessageReaction, Message } = require("discord.js");
+const { or } = require("sequelize/types/lib/operators");
 const { token } = require("./token.json");
+
+const commands = {
+    sneed: {
+        condition: msg => {
+            return msg.content.toLowerCase().includes("sneed");
+        },
+        action: msg => {
+            msg.channel.send("https://i.postimg.cc/ncr1T9ZN/image.png");
+        }
+    }
+}
 
 const bot = new Client();
 
@@ -9,6 +21,12 @@ bot.on("ready", () => {
 
 bot.on("message", (message) => {
     if (message.author.bot) return;
+
+    Object.values(commands).forEach(command => {
+        if (command.condition(message)){
+            command.action(message);
+        }
+    });
 });
 
 bot.login(token);
